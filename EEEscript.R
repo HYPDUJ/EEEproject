f1160d91f0c16c8e50065bd33bc4fa9c9194252c
HYPDUJ <37559328+HYPDUJ@users.noreply.github.com>
2018-03-21 09:13:51 +0100

    Add files via upload

---
+++ EEEscript.R
@@ -0,0 +1,7 @@
+# EEE Project
+#  Meta-analysis
+
+install.packages("meta")
+library(meta)
+
+data1 <- read.csv("~/M2-ERNA/Spring Term/Empirical Environmental Econ/Meta-analysis/RESULTS PAPER CLIMATE AND HEALTH - Feuille 1.csv")
