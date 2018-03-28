8bd6dbce5c1f2f4d88ab5d8111fdd3406155ce82
HYPDUJ <ramirezgoni.felipe@gmail.com>
2018-03-28 21:03:18 +0200

    graph 2

--- EEEscript.R
+++ EEEscript.R
@@ -53,7 +53,7 @@ lower.95 <- data1$beta_norm - 2*data1$se_norm
 data1$lower.95 <- lower.95
 
 
-  data1 <- data1[,c(1,2,3,4,5,8,9,11,12,10,6,7)] # reordering the dataset
+  data1 <- data1[,c(1,2,3,4,5,8,9,11,12,13,10,6,7)] # reordering the dataset
 
 
 
@@ -65,7 +65,7 @@ se_norm_bar <- sqrt(sum(data1$se_norm^2, na.rm = T)) #average se_norm
 upper.95 <- beta_norm_bar + 2*se_norm_bar
 lower.95 <- beta_norm_bar - 2*se_norm_bar
 
-df <- data.frame("Bbar",NA,NA,NA,NA,beta_norm_bar,se_norm_bar,upper.95,lower.95,NA,NA,NA)
+df <- data.frame(NA,"Bbar",NA,NA,NA,NA,beta_norm_bar,se_norm_bar,upper.95,lower.95,NA,NA,NA)
 names(df) <- names(data1)
 
 data1 <- rbind(data1,df)
@@ -77,10 +77,16 @@ data1 <- rbind(data1,df)
 data1 <- data1[order(beta_norm),] #sorting betas in increasing order
 
 A <- ggplot() + 
-    geom_errorbar(data=data1, mapping=aes(x=data1$paper.id, ymin=data1$lower.95, ymax=data1$upper.95), width=0.3, size=0.5, color="blue") + 
-    geom_point(data=data1, mapping=aes(x=data1$paper.id, y=data1$beta_norm), size=3, shape=21, fill="blue") +
-    geom_errorbar(data=data1, mapping=aes(x="Bbar", ymin=data1[which(data1$paper.id=="Bbar"),9], ymax=data1[which(data1$paper.id=="Bbar"),8]), width=0.4, size=0.5, color="red")  +
-    geom_point(data=data1, mapping=aes(x="Bbar", y=data1[which(data1$paper.id=="Bbar"),6]), size=4, shape=21, fill="red") 
+    geom_errorbar(data=data1, mapping=aes(x=data1$Author.s.initials, ymin=data1$lower.95, ymax=data1$upper.95), width=0.3, size=0.5, color="blue") + 
+    geom_point(data=data1, mapping=aes(x=data1$Author.s.initials, y=data1$beta_norm), size=3, shape=21, fill="blue") +
+    geom_errorbar(data=data1, mapping=aes(x="Bbar", ymin=data1[which(data1$paper.id=="Bbar"),10], ymax=data1[which(data1$paper.id=="Bbar"),9]), width=0.4, size=0.5, color="red")  +
+    geom_point(data=data1, mapping=aes(x="Bbar", y=data1[which(data1$paper.id=="Bbar"),7]), size=4, shape=21, fill="red") 
 
 A <- A + theme_economist()  
 A  
+
+  ###   GRAPH 2 ###
+B <- ggplot() +
+  geom_point(data=data1, mapping=aes(x=data1$beta_norm, y=1/data1$se_norm), size=3, shape=21, fill="blue")
+B <- B + theme_economist()
+B
